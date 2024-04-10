eb9c2f6cbd8e40096fffbaafc50b61d7b75cdf15
Stoica-David <stoicadavidioan@gmail.com>
2024-04-10 19:12:10 +0300

    Created base structure of the app.

---
+++ CheckersGame/CheckersGame/ViewModels/MainWindowVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CheckersGame.ViewModels
+{
+    public class MainWindowVM : BaseVM
+    {
+        private BaseVM selectedVM;
+        public BaseVM SelectedVM
+        {
+            get
+            {
+                return selectedVM;
+            }
+            set
+            {
+                selectedVM = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MenuVM MenuViewModel { get; set; }
+
+        public MainWindowVM()
+        {
+            switchToMenu();
+        }
+
+        public void switchToMenu()
+        {
+            MenuViewModel = new MenuVM();
+            SelectedVM = MenuViewModel;
+        }
+    }
+}
---
+++ CheckersGame/CheckersGame/ViewModels/MenuVM.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CheckersGame.ViewModels
+{
+    public class MenuVM : BaseVM
+    {
+        // COMMAND
+
+        private ICommand switchToLoginCommand;
+        public ICommand SwitchToLoginCommand
+        {
+            get
+            {
+                if (switchToLoginCommand == null)
+                {
+                    switchToLoginCommand = new RelayCommand(o => true, o => { OnSwitchToLogin(); });
+                }
+
+                return switchToLoginCommand;
+            }
+        }
+
+        private ICommand switchToSearchCommand;
+        public ICommand SwitchToSearchCommand
+        {
+            get
+            {
+                if (switchToSearchCommand == null)
+                {
+                    switchToSearchCommand = new RelayCommand(o => true, o => { OnSwitchToSearch(); });
+                }
+
+                return switchToSearchCommand;
+            }
+        }
+
+        private ICommand switchToGameCommand;
+        public ICommand SwitchToGameCommand
+        {
+            get
+            {
+                if (switchToGameCommand == null)
+                {
+                    switchToGameCommand = new RelayCommand(o => true, o => { OnSwitchToGame(); });
+                }
+
+                return switchToGameCommand;
+            }
+        }
+
+        // DELEGATES
+
+        public delegate void SwitchToLogin();
+        public SwitchToLogin OnSwitchToLogin { get; set; }
+
+        public delegate void SwitchToSearch();
+        public SwitchToSearch OnSwitchToSearch { get; set; }
+
+        public delegate void SwitchToGame();
+        public SwitchToSearch OnSwitchToGame { get; set; }
+
+        public MenuVM()
+        {
+
+        }
+    }
+}
